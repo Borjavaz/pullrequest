6d110e3b1a6aba560a088875a5af5b9dddf98653
Borja De Saa <bdesaavazquez@danielcastelao.org>
2025-01-24 08:52:38 +0100

    exp 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola pullrequest");
         System.out.println("nueva caracteristica");
+        System.out.println("Finalizada caracteristica");
     }
 }
